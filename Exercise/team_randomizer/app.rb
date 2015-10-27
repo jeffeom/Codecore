4ad4b102e3d6497aca0b87aef639218df2cd4d6a
Jeff Eom <jeffeom@gmail.com>
2015-10-27 16:14:57 -0700

    Edit Ruby on Rails

--- Exercise/team_randomizer/app.rb
+++ Exercise/team_randomizer/app.rb
@@ -8,8 +8,21 @@ get "/" do
 end
 
 post "/" do
-  @params = params
-  @answer = 
-  #erb(:team_randomizer, {layout: :app_layout})
-  erb(:thank_you, {layout: :app_layout})
+
+    @team_names = params["team_names"]
+    team_array = @team_names.split(",")
+
+    @team_number = params["number_of_teams"].to_i
+    #action: team_number
+
+    people = (team_array.count / @team_number.to_f).ceil
+    @teams = Array.new
+
+    (1..@team_number).each do |num|
+      @teams[num-1] = team_array.sample(people)
+      team_array = team_array - @teams[num-1]
+    end
+
+    erb(:team_randomizer, {layout: :app_layout})
+    #erb(:thank_you, {layout: :app_layout})
 end
---
+++ team_randomizer.final/app.rb
@@ -0,0 +1,27 @@
+require "sinatra"
+require "sinatra/reloader"
+
+enable :session
+
+get "/" do
+  erb(:team_randomizer, {layout: :app_layout})
+end
+
+post "/" do
+  @team_names = params["team_names"]
+  team_array = @team_names.split(",")
+
+  @team_number = params["number_of_teams"].to_i
+  #action: team_number
+
+  people = (team_array.count / @team_number.to_f).ceil
+  @teams = Array.new
+
+  (1..@team_number).each do |num|
+    @teams[num-1] = team_array.sample(people)
+    team_array = team_array - @teams[num-1]
+  end
+
+  erb(:team_randomizer, {layout: :app_layout})
+  #erb(:thank_you, {layout: :app_layout})
+end
