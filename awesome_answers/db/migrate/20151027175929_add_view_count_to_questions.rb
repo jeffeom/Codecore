43853899d84fa609ab782eab2db72560b8169d70
Jeff Eom <jeffeom@gmail.com>
2015-11-09 12:40:46 -0800

    Making another repo for awesomeanswers

--- awesome_answers/db/migrate/20151027175929_add_view_count_to_questions.rb
+++
@@ -1,7 +0,0 @@
-# bin/rails g migration add_view_count_to_questions
-
-class AddViewCountToQuestions < ActiveRecord::Migration
-  def change
-    add_column :questions, :view_count, :integer
-  end
-end
--- awesome_answers/db/migrate/20151105211407_add_user_reference_to_questions.rb
+++
@@ -1,6 +0,0 @@
-class AddUserReferenceToQuestions < ActiveRecord::Migration
-  def change
-    add_reference :questions, :user, index: true, foreign_key: true
-    add_column :questions, :abc, :string
-  end
-end
